5271848e30f7daac8aa986bfb3de9603ab3d5bf2
Andrew Le <hieultph@gmail.com>
2024-07-07 13:31:55 +0700

    Done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModel = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
